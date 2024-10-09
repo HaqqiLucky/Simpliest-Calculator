fa256cfae54e0e7eadc6a1cb40422ca1733b829f
HaqqiLucky <haqqiarifin@outlook.com>
2024-10-10 05:15:34 +0700

    pake append buat nambahin ke edittext

--- app/src/main/java/com/example/kalkulatortersimplesedunia/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatortersimplesedunia/MainActivity.kt
@@ -14,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 //        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -22,5 +23,35 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        binding.siji.setOnClickListener {
+            binding.textviewoperasibilangan.append("1")
+        }
+        binding.loro.setOnClickListener {
+            binding.textviewoperasibilangan.append("2")
+        }
+        binding.telu.setOnClickListener {
+            binding.textviewoperasibilangan.append("3")
+        }
+        binding.sekawan.setOnClickListener {
+            binding.textviewoperasibilangan.append("4")
+        }
+        binding.gangsal.setOnClickListener {
+            binding.textviewoperasibilangan.append("5")
+        }
+        binding.enem.setOnClickListener {
+            binding.textviewoperasibilangan.append("6")
+        }
+        binding.pitu.setOnClickListener {
+            binding.textviewoperasibilangan.append("7")
+        }
+        binding.wolu.setOnClickListener {
+            binding.textviewoperasibilangan.append("8")
+            }
+        binding.songo.setOnClickListener {
+            binding.textviewoperasibilangan.append("9")
+            }
+        binding.nol.setOnClickListener {
+            binding.textviewoperasibilangan.append("0")
+            }
+        }
     }
-}
